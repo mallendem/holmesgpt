76e32616a6b0bcc79422da6ccf1038e203b7b113
arik <alon.arik@gmail.com>
2025-11-25 12:36:16 +0200

    Aws mcp instructions (#1084)

--- holmes/main.py
+++ holmes/main.py
@@ -10,6 +10,7 @@ if add_custom_certificate(ADDITIONAL_CERTIFICATE):
 # DO NOT ADD ANY IMPORTS OR CODE ABOVE THIS LINE
 # IMPORTING ABOVE MIGHT INITIALIZE AN HTTPS CLIENT THAT DOESN'T TRUST THE CUSTOM CERTIFICATE
 import sys
+from holmes.utils.colors import USER_COLOR
 import json
 import logging
 import socket
@@ -41,7 +42,6 @@ from holmes.utils.console.consts import system_prompt_help
 from holmes.utils.console.logging import init_logging
 from holmes.utils.console.result import handle_result
 from holmes.utils.file_utils import write_json_file
-from holmes.utils.colors import USER_COLOR
 
 app = typer.Typer(add_completion=False, pretty_exceptions_show_locals=False)
 investigate_app = typer.Typer(
